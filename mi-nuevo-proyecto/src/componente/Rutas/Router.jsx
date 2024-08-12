c6e6220568f0d16ec9b1815434507415df72c53f
JorgeDerlis <jorge.d.beltran.o@gmail.com>
2024-08-12 15:03:31 -0400

    Adicion de Rutas
    se añadio un archivo de rutas

---
+++ mi-nuevo-proyecto/src/componente/Rutas/Router.jsx
@@ -0,0 +1,27 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
+import React from 'react'
+export const Router = () => {
+
+    return (
+        <Router>
+            <Routes>
+
+
+
+            </Routes>
+
+
+
+
+        </Router>
+
+
+
+    )
+
+
+}
